efe5d926d00ef745406b72fea2a5c1b49c6a82d9
sandeep <gautamsandeep2021@gmail.com>
2024-08-08 18:52:44 +0530

    add table resposive

--- pnr_status.js
+++ pnr_status.js
@@ -1,5 +1,6 @@
 document.getElementById("search-btn").addEventListener("click", async () => {
   const loader = document.getElementById("loader");
+  const resultDiv = document.getElementById("result");
   loader.style.display = "block";
   const pnrNumber = document.getElementById("pnr-input").value;
   const url = `https://irctc-indian-railway-pnr-status.p.rapidapi.com/getPNRStatus/${pnrNumber}`;
@@ -10,82 +11,85 @@ document.getElementById("search-btn").addEventListener("click", async () => {
       "x-rapidapi-host": "irctc-indian-railway-pnr-status.p.rapidapi.com",
     },
   };
-  document.getElementById("result").innerHTML = "";
-
   try {
+    resultDiv.innerHTML = ""
     const response = await fetch(url, options);
-    const data = await response.json();
+    const jsonData = await response.json();
+    const data = jsonData.data;
     loader.style.display = "none";
     let tableContainer = document.createElement("div");
 
     tableContainer.classList.add("table-container");
     tableContainer.innerHTML = `<table id="train-details">
-            <thead>
-                <tr>
-                    <th>Train Number</th>
-                    <th>Train Name</th>
-                    <th>Boarding Date</th>
-                    <th>From</th>
-                    <th>To</th>
-                    <th>Reserved Up To</th>
-                    <th>Boarding Point</th>
-                    <th>Class</th>
-                </tr>
-            </thead>
-            <tbody>
-            <tr>
-             <td>${data.trainNumber}</td>
-            <td>${data.trainName}</td>
-            <td>${data.dateOfJourney}</td>
-            <td>${data.boardingPoint}</td>
-            <td>${data.destinationStation}</td>
-            <td>${data.destinationStation}</td>
-            <td>${data.boardingPoint}</td>
-            <td>${data.journeyClass}</td>
-            </tr>
-            </tbody>
-        </table>
-
-        <table id="booking-status">
-            <thead>
-                <tr>
-                    <th>S No</th>
-                    <th>Booking Status</th>
-                    <th>Current Status</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-             ${data.passengerList.map((passenger) => {
-               return ` <tr><td>Passenger ${passenger.passengerSerialNumber}</td>
-               <td>${passenger.bookingStatusDetails}/${passenger.passengerQuota}</td>
-               <td>${passenger.currentStatusDetails}</td></tr> `;
-             })}
-           
-            
-            </tbody>
-        </table>
-
-        <table id="fare-chart">
-            <thead>
-                <tr>
-                    <th>Total Fare</th>
-                    <th>Chart Status</th>
-                </tr>
-            </thead>
-            <tbody> <tr>
-            <td>${data.ticketFare}</td>
-            <td>${data.chartStatus}</td>
-           
-            </tr></tbody>
-        </table>`;
-    document.getElementById("result").append(tableContainer);
+              <thead>
+                  <tr>
+                      <th>Train Number</th>
+                      <th>Train Name</th>
+                      <th>Boarding Date</th>
+                      <th>From</th>
+                      <th>To</th>
+                      <th>Reserved Up To</th>
+                      <th>Boarding Point</th>
+                      <th>Class</th>
+                  </tr>
+              </thead>
+              <tbody>
+              <tr>
+               <td>${data.trainNumber}</td>
+              <td>${data.trainName}</td>
+              <td>${data.dateOfJourney}</td>
+              <td>${data.boardingPoint}</td>
+              <td>${data.destinationStation}</td>
+              <td>${data.destinationStation}</td>
+              <td>${data.boardingPoint}</td>
+              <td>${data.journeyClass}</td>
+              </tr>
+              </tbody>
+          </table>
+  
+          <table id="booking-status">
+              <thead>
+                  <tr>
+                      <th>S No</th>
+                      <th>Booking Status</th>
+                      <th>Current Status</th>
+                  </tr>
+              </thead>
+              <tbody>
+              
+               ${data.passengerList
+                 .map((passenger) => {
+                   return ` <tr><td>Passenger ${passenger.passengerSerialNumber}</td>
+                 <td>${passenger.bookingStatusDetails}/${passenger.passengerQuota}</td>
+                 <td>${passenger.currentStatusDetails}</td></tr> `;
+                 })
+                 .join("")}
+             
+              
+              </tbody>
+          </table>
+  
+          <table id="fare-chart">
+              <thead>
+                  <tr>
+                      <th>Total Fare</th>
+                      <th>Chart Status</th>
+                  </tr>
+              </thead>
+              <tbody> <tr>
+              <td>${data.ticketFare}</td>
+              <td>${data.chartStatus}</td>
+             
+              </tr></tbody>
+          </table>`;
+    resultDiv.append(tableContainer);
 
     // Clear previous results
   } catch (error) {
     loader.style.display = "none";
-    document.getElementById("result").innerText =
-      "Error fetching data. Please try again.";
+    resultDiv.innerHTML = (
+      "<span id='error'>Please fill correct pnr number</span>"
+    );
     console.error(error);
   }
 });
--- script.js
+++ script.js
@@ -9,7 +9,6 @@ document
       .value.toUpperCase();
     const journeyDate = document.getElementById("journeyDate").value;
     const quota = document.getElementById("quota").value;
-    console.log(quota);
     if (departureStation && arrivalStation && journeyDate) {
       const url = `https://irctc1.p.rapidapi.com/api/v3/trainBetweenStations?fromStationCode=${departureStation}&toStationCode=${arrivalStation}&dateOfJourney=${journeyDate}`;
       const options = {
@@ -31,8 +30,10 @@ document
         if (result.data && result.data.length > 0) {
           result.data.forEach((train) => {
             console.log(train);
-            resultDiv.innerHTML += `
-                        <div class="card">  
+            let card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                     
                         <div class="row-first">
                            <div class="train-name"><h2> ${train.train_name} (${
               train.train_number
@@ -40,7 +41,9 @@ document
                            <div class="train-run">Runs On:${train.run_days.map(
                              (days) => days
                            )}</div>
-                           <div class="train-schedule"><a href="">Train Schedule</a></div>
+                           <div class="train-schedule" onClick="showSchedule(${
+                             train.train_number
+                           })">Train Schedule</div>
                            </div>
                        </div>
                        <div class="row-second">
@@ -51,21 +54,26 @@ document
                             <div class="duration">------ ${
                               train.duration
                             } ------</div>
-                            <div class="to-station-details">${train.to_sta}| ${
+                            <div class="to-station-details">${train.to_std}| ${
               train.to_station_name
             }|</div> </div>
                         </div>
                        <div class="row-third">
-                       <div class="class-type">${train.class_type.map(
-                         (type) =>
-                           ` <div class="box" onClick="CheckAvailabeSeat(${train})" >${type}</div>`
-                       )}</div>
+                       <div class="class-type">${train.class_type
+                         .map(
+                           (type) =>
+                             ` <div class="box" onClick='CheckAvailableSeat(event,${JSON.stringify(
+                               train
+                             )})' >${type}</div>`
+                         )
+                         .join("")}</div>
                       </div>
                        <div class="book-btn">
                        <button>Book Now</button>
                          </div>                  
-                        </div>
+                      
                     `;
+            resultDiv.appendChild(card);
           });
         } else {
           resultDiv.innerHTML =
@@ -81,7 +89,44 @@ document
         '<p class="error-message">Please enter departure station, arrival station, and date of journey.</p>';
     }
   });
+// get train schedule .....................................
+async function showSchedule(trainNumber) {
+  const url = `https://irctc1.p.rapidapi.com/api/v1/getTrainSchedule?trainNo=${trainNumber}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "7ee1ff6601msh3cab77fcefd2954p1eae34jsnfd842a6670ba",
+      "x-rapidapi-host": "irctc1.p.rapidapi.com",
+    },
+  };
 
-function CheckAvailabeSeat(e) {
-  console.log(e);
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+// checkAvailableSeat ..................................
+async function CheckAvailableSeat(event, train) {
+  event = event || window.event;
+  console.log(event);
+
+  const url = `https://irctc1.p.rapidapi.com/api/v1/checkSeatAvailability?classType=${event.target.value}&fromStationCode=${train.from}&quota=GN&toStationCode=${train.to}&trainNo=${train.train_number}&date=${journeyDate}'`;
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "7ee1ff6601msh3cab77fcefd2954p1eae34jsnfd842a6670ba",
+      "x-rapidapi-host": "irctc1.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
